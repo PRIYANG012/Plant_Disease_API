26eddec95dba89600865efdcef14d91b09ab7d12
Lenovo <priyangspatel12345@gmail.com>
2021-03-10 23:21:05 +0530

    ALL Models

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 from config import app, api, logger
 
-from resources.Plants import Plant_Prediction,Tomato_Prediction,Apple_Prediction,Cherry_Prediction
+from resources.Plants import Tomato_Prediction,Apple_Prediction,Cherry_Prediction,Corn_Prediction,Grape_Prediction,Peach_Prediction,Pepper_Prediction,Potato_Prediction,Strawberry_Prediction,Tomato_Prediction
 
 """
     Routing
 """
 
-api.add_resource(Plant_Prediction, '/api/Prediction', endpoint='Plant_Prediction')
 api.add_resource(Apple_Prediction, '/api/Apple_Prediction', endpoint='Apple_Prediction')
-
-api.add_resource(Tomato_Prediction, '/api/Tomato_Prediction', endpoint='Tomato_Prediction')
 api.add_resource(Cherry_Prediction, '/api/Cherry_Prediction', endpoint='Cherry_Prediction')
+api.add_resource(Corn_Prediction, '/api/Corn_Prediction', endpoint='Corn_Prediction')
+api.add_resource(Grape_Prediction, '/api/Grape_Prediction', endpoint='Grape_Prediction')
+api.add_resource(Peach_Prediction, '/api/Peach_Prediction', endpoint='Peach_Prediction')
+api.add_resource(Pepper_Prediction, '/api/Pepper_Prediction', endpoint='Pepper_Prediction')
+api.add_resource(Potato_Prediction, '/api/Cherry_Prediction', endpoint='Potato_Prediction')
+api.add_resource(Strawberry_Prediction, '/api/Strawberry_Prediction', endpoint='Strawberry_Prediction')
+api.add_resource(Tomato_Prediction, '/api/Tomato_Prediction', endpoint='Tomato_Prediction')
+
 
 
 
--- resources/Plants.py
+++ resources/Plants.py
@@ -1,9 +1,7 @@
 from config import app, logger
 from flask import request, jsonify
 from flask_restful import Resource
-
 import datetime
-
 import numpy as np
 import pickle
 import cv2
@@ -24,222 +22,31 @@ from keras.preprocessing import image
 from keras.preprocessing.image import img_to_array,save_img
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.model_selection import train_test_split
-
 import base64
 from io import BytesIO
 from PIL import Image
 
-
-
-
-def convert_image_to_array(image_dir):
-    try:
-        image = cv2.imread(image_dir)
-        if image is not None:
-            image = cv2.resize(image, DEFAULT_IMAGE_SIZE)   
-            return img_to_array(image)
-        else:
-            return np.array([])
-    except Exception as e:
-        print(f"Error : {e}")
-        return None
-
-
-# image loading and converting to array
-def load_image_for_general(img_path, show=False):
-    img = cv2.resize(img_path, (128, 128))
-    img_tensor = image.img_to_array(img)                    # (height, width, channels)
-    img_tensor = np.expand_dims(img_tensor, axis=0)         # (1, height, width, channels), add a dimension because the model expects this shape: (batch_size, height, width, channels)
-   
-    return img_tensor
-
-
-
-# image loading and converting to array
-def load_image(img_path, show=False):
-    img = cv2.resize(img_path, (224, 224))
-    img_tensor = image.img_to_array(img)                    # (height, width, channels)
-    img_tensor = np.expand_dims(img_tensor, axis=0)         # (1, height, width, channels), add a dimension because the model expects this shape: (batch_size, height, width, channels)
-   
-    return img_tensor
-
-
-
-
-# Prediction Class
-class Plant_Prediction(Resource):
-
-    def post(self):
+#Tomato Prediction Class
+class Tomato_Prediction(Resource):
+    def get(self):
         try:
-
-            data = request.json
-            
-            # model = load_model("Model/Final_Model_Plant.h5")
-            model2 = load_model("Model/Adam_Final11.h5")
-            
-
-            im_b64=data["image"]
-            im_bytes = base64.b64decode(im_b64)
-            # im_arr is one-dim Numpy array
-            im_arr = np.frombuffer(im_bytes, dtype=np.uint8)  
-            # converted to image object not stored locally
-            img = cv2.imdecode(im_arr, flags=cv2.IMREAD_COLOR)
-            # image path
-            img_path = img 
-           
-
-            # load a single image
-            new_image = load_image_for_general(img)
-
-           
-            # check prediction
-            pred2 = model2.predict(new_image,batch_size=1)
-
-            print(pred2)
-
-
-            preds=np.argmax(pred2, axis=1)
-
-
-            if data["plant"]=="Apple":
-                # do something
-                if preds==0:
-                    preds="Apple scab"
-                elif preds==1:
-                    preds="Apple Black Rot"
-                elif preds==2:
-                    preds="Cedar Apple Rust"
-                elif preds==3:
-                    preds="Healthy Apple"
-            elif data["plant"]=="Blueberry":
-                if preds==4:
-                    preds="Healthy Blueberry"
-                 
-                #do something
-            elif data["plant"]=="Cherry":
-                if preds==5:
-                    preds="Healthy Cherry"
-                elif preds==6:
-                    preds="Cherry Powdery Mildew"
-                # do something
-            elif data["plant"]=="Corn":
-                if preds==7:
-                    preds="Corn (maize) Cercospora leaf spot Gray leaf spot"
-                elif preds==8:
-                    preds="Corn Comman Rust"
-                elif preds==9:
-                    preds="Healthy Corn"
-                elif preds==10:
-                    preds="Corn Northen leaf blight"
-                # do something
-            elif data["plant"]=="Grape":
-                if preds==11:
-                    preds="Grape black Rot"
-                elif preds==12:
-                    preds="Grape Esca (Black Meales)"
-                elif preds==13:
-                    preds="Grape Leaf Blight"
-                elif preds==14:
-                    preds="Healthy Grape"
-                #do something
-            elif data["plant"]=="Orange":
-                if preds==15:
-                    preds="Orange Haunglongbing (Citrus_greening)"
-               
-            elif data["plant"]=="Peach":
-                if preds==16:
-                    preds="Bacterial spot"
-                elif preds==17:
-                    preds="Healthy Peach"
-                #do something
-            elif data["plant"]=="Pepper":
-                if preds==18:
-                    preds="Bacterial spot"
-                elif preds==19:
-                    preds="Healthy Pepper Bell"
-                # do something
-            elif data["plant"]=="Potato":
-                if preds==20:
-                    preds="Healthy Potato"
-                elif preds==21:
-                    preds="Early blight"
-                elif preds==22:
-                    preds="Late blight"
-                #do something
-            elif data["plant"]=="Raspberry":
-                if preds==23:
-                    preds="Healthy Raspberry"
-                
-                # do something
-            elif data["plant"]=="Soyabean":
-                if preds==24:
-                    preds="Soyabean Healthy"
-                
-                #do something
-            elif data["plant"]=="Squash":
-                if preds==25:
-                    preds="Squash Powdery mildew"
-                   
-                # do something
-            elif data["plant"]=="Strawberry":
-                if preds==26:
-                    preds="Healthy Strawberry"
-                elif preds==27:
-                    preds="Strawberry Leaf Scorch"
-                #do something
-            else:   #for Tomato
-                if preds==28:
-                    preds="Bacterial_spot"
-                elif preds==29:
-                    preds="Early_blight"
-                elif preds==30:
-                    preds="Late_blight"
-                elif preds==31:
-                    preds="Leaf_Mold"
-                elif preds==32:
-                    preds="Septoria_leaf_spot"
-                elif preds==33:
-                    preds="Spider_mites Two-spotted_spider_mite"
-                elif preds==34:
-                    preds="Target_Spot"
-                elif preds==35:
-                    preds="Tomato_Yellow_Leaf_Curl_Virus"
-                elif preds==36:
-                    preds="Tomato_mosaic_virus"
-                else:
-                    preds="Healthy"
-
-
-                # do something
-            
-
-
-
+            msg="It's working perfectly"
             return{
-                "Predicted result":preds
-               
+                "Status":msg,
+                
             },201
-            
-
+        
         except Exception as e:
             logger.exception(e)
             return {
                 "msg": "Internal Error"
             }, 500
-
-
-
-
-#Tomato Prediction Class
-class Tomato_Prediction(Resource):
-
+        
     def post(self):
         try:
 
             data = request.json
-            Model_Apple = load_model("Model/Apple_model_inception.h5")
-            # with open('Testing Images/healthy.JPG', "rb") as img_file:
-            #     im_b64=base64.b64encode(img_file.read()).decode('utf-8')
+            Model_Apple = load_model("Model/Tomato_model_inception.h5")
             im_b64=data["image"]
             im_bytes = base64.b64decode(im_b64)   # im_bytes is a binary image
             im_file = BytesIO(im_bytes)  # convert image to file-like object
@@ -254,31 +61,30 @@ class Tomato_Prediction(Resource):
             preds=np.argmax(preds, axis=1)
 
             if preds==0:
-                preds="Bacterial_spot"
-                remedies="Once leaf spot has infected your plants, spray with copper fungicide for seven to 10 days. After that, continue to treat every 10 days when weather is dry or every five to seven days when weather is wet. Copper fungicide can also be used preventively after sowing seeds but before moving plants into the garden.Preventive treatments are recommended, as the loss resulting from a bacterial leaf spot infection can be devastating. In addition to preventive copper fungicide treatments, gardeners should ensure their seeds are certified disease-free and soil is sterile, whether you sterilize your own soil or purchase commercial soils. If seeds aren’t sterile, soak them in 1.3% sodium hypochlorite for one minute to sterilize them on your own. Crop rotation and avoiding too-wet conditions are other strategies to prevent leaf spot. Opt for drip irrigation, or water plants at their base instead of from overhead, and do your watering in the morning instead of later in the day."
+                preds="Tomato___Bacterial_spot"
             elif preds==1:
-                preds="Early_blight"
+                preds="Tomato___Early_blight"
             elif preds==2:
-                preds="Late_blight"
+                preds="Tomato___Late_blight"
             elif preds==3:
-                preds="Leaf_Mold"
+                preds="Tomato___healthy"
             elif preds==4:
-                preds="Septoria_leaf_spot"
+                preds="Tomato___Leaf_Mold"
             elif preds==5:
-                preds="Spider_mites Two-spotted_spider_mite"
+                preds="Tomato___Septoria_leaf_spot"
             elif preds==6:
-                preds="Target_Spot"
+                preds="Tomato___Spider_mites Two-spotted_spider_mite"
             elif preds==7:
-                preds="Tomato_Yellow_Leaf_Curl_Virus"
+                preds="Tomato___Target_Spot"
             elif preds==8:
-                preds="Tomato_mosaic_virus"
+                preds="Tomato___Tomato_mosaic_virus"
             else:
-                preds="Healthy"
-                remedies="If you’re trying to grow the world’s biggest tomato and you have the time to remove all the suckers as your tomato plant grows, then you might want to go ahead and sucker your plants (and be sure to sanitize your tools as you go so you don’t spread diseases)."
+                preds="Tomato___Tomato_Yellow_Leaf_Curl_Virus"
 
             return{
                 "Predicted result":preds,
-                "remedies":remedies
+                
+                
             },201
             
 
@@ -299,8 +105,6 @@ class Apple_Prediction(Resource):
 
             data = request.json
             Model_Apple = load_model("Model/Apple_model_inception.h5")
-            # with open('Testing Images/healthy.JPG', "rb") as img_file:
-            #     im_b64=base64.b64encode(img_file.read()).decode('utf-8')
             im_b64=data["image"]
             im_bytes = base64.b64decode(im_b64)   # im_bytes is a binary image
             im_file = BytesIO(im_bytes)  # convert image to file-like object
@@ -348,8 +152,6 @@ class Cherry_Prediction(Resource):
 
             data = request.json
             Model_Apple = load_model("Model/Cherry_model_inception.h5")
-            # with open('Testing Images/healthy.JPG', "rb") as img_file:
-            #     im_b64=base64.b64encode(img_file.read()).decode('utf-8')
             im_b64=data["image"]
             im_bytes = base64.b64decode(im_b64)   # im_bytes is a binary image
             im_file = BytesIO(im_bytes)  # convert image to file-like object
@@ -367,6 +169,273 @@ class Cherry_Prediction(Resource):
                 pass_preds="Cherry_(including_sour)___Powdery_mildew"
             elif preds==1:
                 pass_preds="Cherry_(including_sour)___healthy"
+            
+
+
+            return{
+                "Predicted result":pass_preds
+                
+            },201
+            
+
+        except Exception as e:
+            logger.exception(e)
+            return {
+                "msg": "Internal Error"
+            }, 500
+
+
+
+
+
+#Corn Prediction Class
+class Corn_Prediction(Resource):
+
+    def post(self):
+        try:
+
+            data = request.json
+            Model_Apple = load_model("Model/Corn_model_inception.h5")
+            im_b64=data["image"]
+            im_bytes = base64.b64decode(im_b64)   # im_bytes is a binary image
+            im_file = BytesIO(im_bytes)  # convert image to file-like object
+            img = Image.open(im_file)   # img is now PIL Image object
+            img = img.resize((224, 224)) 
+            # Preprocessing the image
+            x = image.img_to_array(img)
+            ## Scaling
+            x=x/255
+            x = np.expand_dims(x, axis=0)
+            preds = Model_Apple.predict(x)
+            preds=np.argmax(preds, axis=1)
+
+            if preds==0:
+                pass_preds="Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot"
+            elif preds==1:
+                pass_preds="Corn_(maize)___Northern_Leaf_Blight"
+            elif preds==2:
+                pass_preds="Corn_(maize)___healthy"
+            elif preds==3:
+                pass_preds="Corn_(maize)___Common_rust_"
+            
+
+
+            return{
+                "Predicted result":pass_preds
+                
+            },201
+            
+
+        except Exception as e:
+            logger.exception(e)
+            return {
+                "msg": "Internal Error"
+            }, 500
+
+
+
+
+#Grape Prediction Class
+class Grape_Prediction(Resource):
+
+    def post(self):
+        try:
+
+            data = request.json
+            Model_Apple = load_model("Model/Grape_model_inception.h5")
+            im_b64=data["image"]
+            im_bytes = base64.b64decode(im_b64)   # im_bytes is a binary image
+            im_file = BytesIO(im_bytes)  # convert image to file-like object
+            img = Image.open(im_file)   # img is now PIL Image object
+            img = img.resize((224, 224)) 
+            # Preprocessing the image
+            x = image.img_to_array(img)
+            ## Scaling
+            x=x/255
+            x = np.expand_dims(x, axis=0)
+            preds = Model_Apple.predict(x)
+            preds=np.argmax(preds, axis=1)
+
+            if preds==0:
+                pass_preds="Grape___Esca_(Black_Measles)"
+            elif preds==1:
+                pass_preds="Grape___healthy"
+            elif preds==2:
+                pass_preds="Grape___Leaf_blight_(Isariopsis_Leaf_Spot)"
+            elif preds==3:
+                pass_preds="Grape___Black_rot"
+            
+            
+
+
+            return{
+                "Predicted result":pass_preds
+                
+            },201
+            
+
+        except Exception as e:
+            logger.exception(e)
+            return {
+                "msg": "Internal Error"
+            }, 500
+
+
+
+#Peach Prediction Class
+class Peach_Prediction(Resource):
+
+    def post(self):
+        try:
+
+            data = request.json
+            Model_Apple = load_model("Model/Peach_model_inception.h5")
+            im_b64=data["image"]
+            im_bytes = base64.b64decode(im_b64)   # im_bytes is a binary image
+            im_file = BytesIO(im_bytes)  # convert image to file-like object
+            img = Image.open(im_file)   # img is now PIL Image object
+            img = img.resize((224, 224)) 
+            # Preprocessing the image
+            x = image.img_to_array(img)
+            ## Scaling
+            x=x/255
+            x = np.expand_dims(x, axis=0)
+            preds = Model_Apple.predict(x)
+            preds=np.argmax(preds, axis=1)
+
+            if preds==0:
+                pass_preds="Peach___Bacterial_spot"
+            elif preds==1:
+                pass_preds="Peach___healthy"
+            
+
+
+            return{
+                "Predicted result":pass_preds
+                
+            },201
+            
+
+        except Exception as e:
+            logger.exception(e)
+            return {
+                "msg": "Internal Error"
+            }, 500
+
+
+
+
+#Pepper Prediction Class
+class Pepper_Prediction(Resource):
+
+    def post(self):
+        try:
+
+            data = request.json
+            Model_Apple = load_model("Model/Pepper_model_inception.h5")
+            im_b64=data["image"]
+            im_bytes = base64.b64decode(im_b64)   # im_bytes is a binary image
+            im_file = BytesIO(im_bytes)  # convert image to file-like object
+            img = Image.open(im_file)   # img is now PIL Image object
+            img = img.resize((224, 224)) 
+            # Preprocessing the image
+            x = image.img_to_array(img)
+            ## Scaling
+            x=x/255
+            x = np.expand_dims(x, axis=0)
+            preds = Model_Apple.predict(x)
+            preds=np.argmax(preds, axis=1)
+
+            if preds==0:
+                pass_preds="Pepper,_bell___Bacterial_spot"
+            elif preds==1:
+                pass_preds="Pepper,_bell___healthy"
+            
+
+
+            return{
+                "Predicted result":pass_preds
+                
+            },201
+            
+
+        except Exception as e:
+            logger.exception(e)
+            return {
+                "msg": "Internal Error"
+            }, 500
+
+
+
+#Potato Prediction Class
+class Potato_Prediction(Resource):
+
+    def post(self):
+        try:
+
+            data = request.json
+            Model_Apple = load_model("Model/Potato_model_inception.h5")
+            im_b64=data["image"]
+            im_bytes = base64.b64decode(im_b64)   # im_bytes is a binary image
+            im_file = BytesIO(im_bytes)  # convert image to file-like object
+            img = Image.open(im_file)   # img is now PIL Image object
+            img = img.resize((224, 224)) 
+            # Preprocessing the image
+            x = image.img_to_array(img)
+            ## Scaling
+            x=x/255
+            x = np.expand_dims(x, axis=0)
+            preds = Model_Apple.predict(x)
+            preds=np.argmax(preds, axis=1)
+
+            if preds==0:
+                pass_preds="Potato___Early_blight"
+            elif preds==1:
+                pass_preds="Potato___healthy"
+            elif preds==2:
+                pass_preds="Potato___Late_blight"
+            
+
+
+            return{
+                "Predicted result":pass_preds
+                
+            },201
+            
+
+        except Exception as e:
+            logger.exception(e)
+            return {
+                "msg": "Internal Error"
+            }, 500
+
+
+
+#Strawberry Prediction Class
+class Strawberry_Prediction(Resource):
+
+    def post(self):
+        try:
+
+            data = request.json
+            Model_Apple = load_model("Model/Strawberry_model_inception.h5")
+            im_b64=data["image"]
+            im_bytes = base64.b64decode(im_b64)   # im_bytes is a binary image
+            im_file = BytesIO(im_bytes)  # convert image to file-like object
+            img = Image.open(im_file)   # img is now PIL Image object
+            img = img.resize((224, 224)) 
+            # Preprocessing the image
+            x = image.img_to_array(img)
+            ## Scaling
+            x=x/255
+            x = np.expand_dims(x, axis=0)
+            preds = Model_Apple.predict(x)
+            preds=np.argmax(preds, axis=1)
+
+            if preds==0:
+                pass_preds="Strawberry___Leaf_scorch"
+            elif preds==1:
+                pass_preds="Strawberry___healthy"
             
 
 
